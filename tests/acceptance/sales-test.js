0d97b4b29583963bd741a2f758610278e7422039
suba2002harini <subaharini2002@gmail.com>
2022-09-22 16:07:57 +0530

    First commit

---
+++ tests/acceptance/sales-test.js
@@ -0,0 +1,15 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'sales/tests/helpers';
+
+module('Acceptance | sales', function (hooks) {
+  setupApplicationTest(hooks);
+  test('visiting /', async function (assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+    assert.dom('h2').hasText('Welcome to Super Rentals!');
+    assert
+  });
+
+});
