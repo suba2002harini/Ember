edb527e308627772dce516db0ce9cecf5d6d2e08
suba2002harini <subaharini2002@gmail.com>
2022-09-27 13:01:55 +0530

    lint changes 4

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,20 +1,17 @@
 import Route from '@ember/routing/route';
 
 const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
-export default class IndexRoute extends Route 
-{
+export default class IndexRoute extends Route {
   async model(){
     let response = await fetch('/api/rentals.json');
     let { data } = await response.json();
     return data.map((model) => {
       let { attributes } = model;
       let type;
-      if (COMMUNITY_CATEGORIES.includes(attributes.category)) 
-      {
+      if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
         type = 'Community';
       } 
-      else 
-      {
+      else {
         type = 'Standalone';
       }
       return { type, ...attributes };
