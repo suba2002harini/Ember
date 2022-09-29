481427a9136f59c7030f0ba9283414a605641fb0
suba2002harini <subaharini2002@gmail.com>
2022-09-29 16:13:38 +0530

    added provided components

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -1,20 +1,23 @@
 import Route from '@ember/routing/route';
 
-const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
+import { inject as service } from '@ember/service';
+// const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
 
 export default class RentalRoute extends Route {
+  //   async model(params) {
+  //     let response = await fetch(`/api/rentals/${params.rental_id}.json`);
+  //     let { data } = await response.json();
+  //     let { id, attributes } = data;
+  //     let type;
+  @service store;
+  // if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
+  //   type = 'Community';
+  // } else {
+  //   type = 'Standalone';
+  // }
+  // console.log(attributes);
+  // return { id, type, ...attributes };
   async model(params) {
-    let response = await fetch(`/api/rentals/${params.rental_id}.json`);
-    let { data } = await response.json();
-    let { id, attributes } = data;
-    let type;
-
-    if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-      type = 'Community';
-    } else {
-      type = 'Standalone';
-    }
-    console.log(attributes);
-    return { id, type, ...attributes };
+    return this.store.findRecord('rental', params.rental_id);
   }
 }
--- tests/acceptance/sales-test.js
+++ tests/acceptance/sales-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { click,visit, currentURL } from '@ember/test-helpers';
+import { find, click, visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'sales/tests/helpers';
 
 module('Acceptance | sales', function (hooks) {
@@ -25,7 +25,16 @@ module('Acceptance | sales', function (hooks) {
     assert.equal(currentURL(), '/rentals/grand-old-mansion');
     assert.dom('nav').exists();
     assert.dom('.rental.detailed').exists();
+    assert.dom('.share.button').hasText('Share on Twitter');
+
+    let button = find('.share.button');
+
+    let tweetURL = new URL(button.href);
+    assert.equal(tweetURL.host, 'twitter.com');
+
+    assert.equal(
+      tweetURL.searchParams.get('url'),
+      `${window.location.origin}/rentals/grand-old-mansion`
+    );
   });
-
-
 });
