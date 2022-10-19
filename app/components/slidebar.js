5f678738b876bdf82aa6ac3f4369c068fa33b110
suba2002harini <subaharini2002@gmail.com>
2022-10-19 11:09:03 +0530

    lint changes

--- app/components/slidebar.js
+++ app/components/slidebar.js
@@ -1,17 +1,5 @@
 import Component from "@glimmer/component";
-import { tracked } from "@glimmer/tracking";
-import { action } from "@ember/object";
+
 
 export default class SidebarComponent extends Component {
-  @tracked showingHelp = false;
-
-  @action
-  showHelp() {
-    this.showingHelp = true;
-  }
-
-  @action
-  hideHelp() {
-    this.showingHelp = false;
-  }
 }
